806811f400702d190081fdf591a68889a96dcf64
Guilherme Souza <guilhermemoraes.dev@gmail.com>
2024-10-01 18:37:44 -0300

    dom 2

---
+++ dom2.js
@@ -0,0 +1,35 @@
+// const items = document.querySelector(".items");
+
+// items.remove();
+// Remover items
+
+const items = document.querySelector(".items");
+
+// console.log(items.firstElementChild);
+// Pega o primeiro filho da UL
+
+// console.log(items.firstElementChild.remove());
+// Remove o primeiro filho da UL
+
+// console.log(items.lastElementChild);
+// Pega o último filho da UL
+
+// console.log(items.lastElementChild.remove());
+// Remove o último filho da UL
+
+// console.log(items.children[1]);
+// Pega o item de index[1] (segundo na fila)
+
+// items.children[1].textContent = "Item Um";
+// Altera o texto do item selecionado
+
+// items.lastElementChild.innerHTML = '<h1>Hello World!</h1>';
+// Altera o HTML do elemento selecionado
+
+const button = document.querySelector(".btn");
+
+button.style.background = "red";
+// Altera o background do elemento
+
+button.style.color = "blue";
+// Altera a cor do elemento
