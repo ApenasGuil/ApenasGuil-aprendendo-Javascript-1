c8c43326093d46fd70da6b405e5fcbff037bc922
Guilherme Souza <guilhermemoraes.dev@gmail.com>
2024-10-01 15:46:15 -0300

    list

--- main.js
+++ main.js
@@ -2,5 +2,34 @@ const names = ["Guilherme", "Kervia", "Alice", 10, false];
 // Listas começam no index 0
 
 const nome = names[1];
+// console.log(nome);
 
-console.log(nome);
+names.push("Gustavo");
+// Adiciona ao final da Lista
+
+names.shift();
+// Remove o index[0] da Lista
+
+names.unshift("Felipe");
+// Adiciona X como index[0], empurrando o resto para trás
+
+names.pop();
+// Remove o último index da Lista
+
+names[3] = "Pedro";
+// Reassinala o index especificado
+
+// names = ['a', 'b', 'c'];
+// Impossível reassinalar diretamente todos os indexes da Lista
+
+// console.log(names.indexOf('Alice'));
+// Exibe o index do termo especificado
+
+console.log(names);
+
+const sortedNames = names.sort();
+console.log(sortedNames);
+
+const namesIsArray = Array.isArray(names);
+// Checa se a variável especificada é um array ou não
+console.log(namesIsArray);
