8f6716197f99d3354be9592d0940daf81e472627
Guilherme Souza <guilhermemoraes.dev@gmail.com>
2024-10-01 17:10:38 -0300

    truthy or falsy

---
+++ truthyorfalsy.js
@@ -0,0 +1,12 @@
+const x = '';
+// '', 0, null e undefined = FALSE
+
+// console.log(!!x);
+
+const list = [];
+console.log(!![]);
+// lista vazia = TRUE
+
+const object = {};
+console.log(!!object);
+// object vazio = TRUE
